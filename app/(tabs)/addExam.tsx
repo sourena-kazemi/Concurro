535dd86f78c8f71df7e6ce7aa124a0be08adb757
sourena-kazemi <kazemisourena@gmail.com>
2025-02-12 22:29:21 +0330

    setup data validation

--- app/(tabs)/addExam.tsx
+++ app/(tabs)/addExam.tsx
@@ -1,17 +1,50 @@
-import { Text, View, TextInput } from "react-native"
+import { Text, View, TextInput, Button } from "react-native"
 import { useState } from "react"
 export default function AddExam() {
 	const [name, setName] = useState("")
-	const [size, setSize] = useState(0)
+	const [size, setSize] = useState(105)
 
-	const [mathStart, setMathStart] = useState(0)
-	const [mathEnd, setMathEnd] = useState(0)
+	const [mathStart, setMathStart] = useState(1)
+	const [mathEnd, setMathEnd] = useState(40)
 
-	const [physicsStart, setPhysicsStart] = useState(0)
-	const [physicsEnd, setPhysicsEnd] = useState(0)
+	const [physicsStart, setPhysicsStart] = useState(41)
+	const [physicsEnd, setPhysicsEnd] = useState(75)
 
-	const [chemistryStart, setChemistryStart] = useState(0)
-	const [chemistryEnd, setChemistryEnd] = useState(0)
+	const [chemistryStart, setChemistryStart] = useState(76)
+	const [chemistryEnd, setChemistryEnd] = useState(105)
+
+	const [error, setError] = useState("")
+
+	const handleSubmit = () => {
+		const numberOfQuestions =
+			mathEnd -
+			mathStart +
+			1 +
+			(physicsEnd - physicsStart + 1) +
+			(chemistryEnd - chemistryStart + 1)
+		if (numberOfQuestions !== size) {
+			setError("Wrong Number of Questions !")
+			return
+		}
+		const mathPhysicsOverlap =
+			mathStart <= physicsEnd && physicsStart <= mathEnd
+		const mathChemistryOverlap =
+			mathStart <= chemistryEnd && chemistryStart <= mathEnd
+		const physicsChemistryOverlap =
+			physicsStart <= chemistryEnd && chemistryStart <= physicsEnd
+
+		if (
+			mathPhysicsOverlap ||
+			mathChemistryOverlap ||
+			physicsChemistryOverlap
+		) {
+			setError("Ranges Overlap !")
+			return
+		}
+
+		setError("")
+	}
+
 	return (
 		<View
 			style={{
@@ -27,46 +60,57 @@ export default function AddExam() {
 			/>
 			<Text>Number of questions</Text>
 			<TextInput
-				placeholder="100"
+				placeholder="105"
+				defaultValue="105"
 				keyboardType="numeric"
 				onChangeText={(newValue) => setSize(+newValue)}
 			></TextInput>
 
 			<Text>Mathematics Range</Text>
 			<TextInput
-				placeholder="0"
+				placeholder="1"
+				defaultValue="1"
 				keyboardType="numeric"
 				onChangeText={(newValue) => setMathStart(+newValue)}
 			></TextInput>
 			<TextInput
 				placeholder="40"
+				defaultValue="40"
 				keyboardType="numeric"
 				onChangeText={(newValue) => setMathEnd(+newValue)}
 			></TextInput>
 
 			<Text>Physics Range</Text>
 			<TextInput
-				placeholder="0"
+				placeholder="41"
+				defaultValue="41"
 				keyboardType="numeric"
 				onChangeText={(newValue) => setPhysicsStart(+newValue)}
 			></TextInput>
 			<TextInput
-				placeholder="40"
+				placeholder="75"
+				defaultValue="75"
 				keyboardType="numeric"
 				onChangeText={(newValue) => setPhysicsEnd(+newValue)}
 			></TextInput>
 
 			<Text>Chemistry Range</Text>
 			<TextInput
-				placeholder="0"
+				placeholder="76"
+				defaultValue="76"
 				keyboardType="numeric"
 				onChangeText={(newValue) => setChemistryStart(+newValue)}
 			></TextInput>
 			<TextInput
-				placeholder="40"
+				placeholder="105"
+				defaultValue="105"
 				keyboardType="numeric"
 				onChangeText={(newValue) => setChemistryEnd(+newValue)}
 			></TextInput>
+
+			<Button title="Submit" onPress={() => handleSubmit()} />
+
+			<Text className="text-red-600">{error}</Text>
 		</View>
 	)
 }
